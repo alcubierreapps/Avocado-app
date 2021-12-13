231fa337f95e05623d6c43467992260762de1f6e
dalcubierre <diegoalcubierre@hotmail.com>
2021-12-13 18:28:52 -0500

    Initial Commit

---
+++ Avocado/AvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoApp.swift
+//  Avocado
+//
+//  Created by Alcubierre Holdings Inc. on 2021-12-13.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
